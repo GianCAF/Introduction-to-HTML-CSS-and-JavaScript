0da655908a4e13aab7efdaa33026f1e902362141
GianCAF <giancaf983@gmail.com>
2026-03-11 13:46:35 -0600

    It was created the reduce method iterates through each employee and accumulates their salaries to calculate the total.

--- matrix methods/employeeDetails/employee_details.js
+++ matrix methods/employeeDetails/employee_details.js
@@ -5,3 +5,8 @@ function displayEmployees() {
         .join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
+
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+}
